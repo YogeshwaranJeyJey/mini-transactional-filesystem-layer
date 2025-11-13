1074955cbd6232183edaf6cc1df39b6f6de356b7
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-13 18:06:28 +0530

    Create main.h file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define PROJECTTITLE    "MINI TRANSACTIONAL FILESYSTEM LAYER"
+#define WIDTH   50
+
+#include <stdio.h>
+#include <string.h>
+
+int doPadding(char* string);
+void displayTransactionalOperations();
+
+#endif
