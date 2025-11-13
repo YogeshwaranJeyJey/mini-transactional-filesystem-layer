f8ffdd0fe0eb28abb6ebf3b7c04a3034fa1b7d07
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-13 18:08:19 +0530

    Create menuPage.h file

---
+++ menuPage.h
@@ -0,0 +1,12 @@
+#ifndef MENUPAGE_H
+#define MENUPAGE_H
+
+#include "main.h"
+
+int doPadding(char* string);
+void displayProjectTitle();
+void displayMainMenu();
+void displayDeveloperOptions();
+void displayOtherOptions();
+
+#endif
