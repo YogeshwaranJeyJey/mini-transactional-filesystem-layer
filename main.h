7b2de340e44b67fe23ceef8ac00cba6a2229a91b
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-14 11:15:33 +0530

    Add Display menu macro definitions

--- main.h
+++ main.h
@@ -2,12 +2,15 @@
 #define MAIN_H
 
 #define PROJECTTITLE    "MINI TRANSACTIONAL FILESYSTEM LAYER"
+#define WELCOMEMSG  "Welcome to ABC Bank!"
+#define MAINMENU    "Main Menu"
+#define TRANSACTIONSMENU    "Transactional Operations"
+#define SYSTEMOPTIONSMENU   "System Operations (Admin only)"
+#define SIMULATECRASHMENU   "Crash Simulation options"
+#define OTHEROPTIONS    "Other"
 #define WIDTH   50
 
 #include <stdio.h>
 #include <string.h>
-
-int doPadding(char* string);
-void displayTransactionalOperations();
 
 #endif
