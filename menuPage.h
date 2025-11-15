ff73cd77929b0da608ff0aa2e3d8b35b2c64b577
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-15 13:32:52 +0530

    Add declaration for userChoiceHandler function

--- menuPage.h
+++ menuPage.h
@@ -1,14 +1,20 @@
 #ifndef MENUPAGE_H
 #define MENUPAGE_H
 
-#include "main.h"
+#define PROJECTTITLE    "MINI TRANSACTIONAL FILESYSTEM LAYER"
+#define WELCOMEMSG  "Welcome to ABC Bank!"
+#define MAINMENU    "Main Menu"
+#define TRANSACTIONSMENU    "Transactional Operations"
+#define SYSTEMOPTIONSMENU   "System Operations (Admin only)"
+#define SIMULATECRASHMENU   "Crash Simulation options"
+#define OTHEROPTIONS    "Other"
 
-int doPadding(char* string);
 void displayProjectTitle();
 void displayMainMenu();
 void displayTransactionalOperations();
 void displaySystemOperations();
 void displayCrashSimulationOptions();
 void displayOtherOptions();
+void userChoiceHandler(int userChoice, int *displayAgain);
 
 #endif
