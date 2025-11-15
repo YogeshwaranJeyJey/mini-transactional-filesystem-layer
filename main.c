4cce31b0a9d81fe875869e2b9a0df423c2aa2e2a
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-15 13:24:38 +0530

    Add userChoiceHandler function

--- main.c
+++ main.c
@@ -1,12 +1,25 @@
+#include "common.h"
 #include "menuPage.h"
 
 int main(){
-    displayProjectTitle();
-    //startRecovery();
-    displayMainMenu();
-    displayTransactionalOperations();
-    displaySystemOperations();
-    displayCrashSimulationOptions();
-    displayOtherOptions();
+    char userChoiceBuffer[MAXCHOICECHAR];
+    int userChoice = 0, displayAgain = 1;
+    while(userChoice != 18){
+        if(displayAgain){
+            displayProjectTitle();
+        }
+        //startRecovery();
+        displayMainMenu();
+        displayTransactionalOperations();
+        displaySystemOperations();
+        displayCrashSimulationOptions();
+        displayOtherOptions();
+        
+        printf("Enter your choice: ");
+        fgets(userChoiceBuffer, sizeof(userChoiceBuffer), stdin);
+        userChoice = strtol(userChoiceBuffer, NULL, MAXCHOICECHAR);
+        
+        userChoiceHandler(userChoice, &displayAgain);
+    }
     return 0;
 }
