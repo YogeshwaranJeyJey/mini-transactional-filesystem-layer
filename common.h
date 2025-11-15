ef28c85de4de2ff575d642081e1902b5d6c36b64
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-15 13:30:44 +0530

    Add common.h file

---
+++ common.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define MAXCHOICECHAR   5
+#define WIDTH   50
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int doPadding(char* string);
+
+#endif
