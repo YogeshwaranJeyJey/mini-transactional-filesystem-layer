cce76fc9b73158b1c133d8d27327eda4a4b2cf2d
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-16 17:44:15 +0530

    Add wal handling function headers for transaction operations

--- logHandler.h
+++ logHandler.h
@@ -3,8 +3,26 @@
 
 #include "bankServices.h"
 
+#define MAXBUFFERSIZE   256
+
 long generateTxnId();
-void writeToWal(accountDetails_t* accDetails, long txnId, char* operation);
-void writeToOriginalFile(accountDetails_t* accDetails);
+void writeCreateToWal(accountDetails_t* accDetails, long txnId);
+void writeUpdateToWal(accountDetails_t* oldAccountDetails, accountDetails_t* newAccountDetails, long txnId);
+void writeDeleteToWal(accountDetails_t* oldAccountDetails, long txnId);
+void writeTransferToWal(accountDetails_t* oldFromAccountDetails, 
+    accountDetails_t* fromAccDetails, accountDetails_t* oldToAccountDetails, 
+    accountDetails_t* toAccountDetails, long txnId);
+void writeAddToOriginalFile(accountDetails_t* accDetails);
+void writeUpdateToOriginalFile(accountDetails_t* newAccountDetails);
+void writeDeleteToOriginalFile(accountDetails_t* accDetails);
+void writeTransferToOriginalFile(accountDetails_t* fromAccDetails, accountDetails_t* toAccDetails);
+void writeCommitToWAL(long txnId);
+int validateAccountDetails(accountDetails_t* accDetails);
+int validateReceiverAccount(accountDetails_t* toAccountDetails, long userEnteredAccNo);
+void printAccountDetails(accountDetails_t* accDetails);
+void updateDetails(accountDetails_t* oldAccDetails);
+void deposit(accountDetails_t* oldAccDetails);
+void withDraw(accountDetails_t* oldAccDetails);
+void transfer(accountDetails_t* fromAccDetails);
 
 #endif
