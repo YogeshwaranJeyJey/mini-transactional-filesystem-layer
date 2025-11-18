b6320625521740f79b66316c5da22d6069af303b
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-18 23:30:24 +0530

    Modify wal function declarations for recovery logic

--- logHandler.h
+++ logHandler.h
@@ -3,24 +3,36 @@
 
 #include "bankServices.h"
 
+int checkValidPassword(char* passwd, char* reTypedPasswd);
+long generateNewAccNumber();
+int validateName(char* name);
+int validateMobileNumber(char *mobile);
+int validateMail(char *mail);
+
+int getNewAccountDetails();
 long generateTxnId();
-void writeCreateToWal(accountDetails_t* accDetails, long txnId);
-void writeUpdateToWal(accountDetails_t* oldAccountDetails, accountDetails_t* newAccountDetails, long txnId);
+
+void writeCreateToWal(accountDetails_t* accDetails, long txnId, int recoveryFlag);
+void writeUpdateToWal(accountDetails_t* oldAccountDetails, accountDetails_t* newAccountDetails, long txnId, int recoveryFlag);
 void writeDeleteToWal(accountDetails_t* oldAccountDetails, long txnId);
 void writeTransferToWal(accountDetails_t* oldFromAccountDetails, 
     accountDetails_t* fromAccDetails, accountDetails_t* oldToAccountDetails, 
-    accountDetails_t* toAccountDetails, long txnId);
+    accountDetails_t* toAccountDetails, long txnId, int recoveryFlag);
+
 void writeAddToOriginalFile(accountDetails_t* accDetails);
 void writeUpdateToOriginalFile(accountDetails_t* newAccountDetails);
 void writeDeleteToOriginalFile(accountDetails_t* accDetails);
 void writeTransferToOriginalFile(accountDetails_t* fromAccDetails, accountDetails_t* toAccDetails);
+
 void writeCommitToWAL(long txnId);
+
 int validateAccountDetails(accountDetails_t* accDetails);
 int validateReceiverAccount(accountDetails_t* toAccountDetails, long userEnteredAccNo);
 void printAccountDetails(accountDetails_t* accDetails);
-void updateDetails(accountDetails_t* oldAccDetails);
-void deposit(accountDetails_t* oldAccDetails);
-void withDraw(accountDetails_t* oldAccDetails);
-void transfer(accountDetails_t* fromAccDetails);
+
+int updateDetails(accountDetails_t* oldAccDetails);
+int deposit(accountDetails_t* oldAccDetails);
+int withDraw(accountDetails_t* oldAccDetails);
+int transfer(accountDetails_t* fromAccDetails);
 
 #endif
