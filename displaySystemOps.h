63f659b7a26b34c98e3da39325d91a19bda8eec9
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-18 11:17:29 +0530

    Add displaySystemOperations functions headers

---
+++ displaySystemOps.h
@@ -0,0 +1,12 @@
+#ifndef DISPLAYSYSTEMOPS_H
+#define DISPLAYSYSTEMOPS_H
+
+#define MAXBUFFERSIZE   256
+#define PASSKEYLENGTH   9
+#define ADMINPASSKEY    "123@" 
+
+void showAllAccounts();
+void showWalLogs();
+void clearWalLogs();
+
+#endif
