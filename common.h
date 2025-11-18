4dd8c61e45021a1506c8110e473083586ed42270
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-18 10:57:01 +0530

    Add trimNewLineFunction() declaration

--- common.h
+++ common.h
@@ -3,11 +3,15 @@
 
 #define MAXCHOICECHAR   5
 #define WIDTH   50
+#define MAXBUFFERSIZE   256
+#define CHECKSUMFILEPATH    "checkSum.txt"
+#define WALFILEPATH "wal.txt"
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 int doPadding(char* string);
+void trimNewLine(char* line);
 
 #endif
