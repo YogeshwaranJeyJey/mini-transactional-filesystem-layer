f40eda09630ace323113deb862dca914e192e476
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-18 23:20:22 +0530

    Add aboutProject() function definition

--- common.c
+++ common.c
@@ -7,3 +7,24 @@ int doPadding(char* string){
 void trimNewLine(char* line){
     line[strcspn(line, "\n")] = '\0';
 }
+
+int isBlank(const char *s){
+    while (*s == ' ' || *s == '\t' || *s == '\n' || *s == '\r') s++;
+    return *s == '\0';
+}
+
+void aboutProject(){
+    FILE* fp = fopen(ABOUT_PROJECT_FILE_PATH, "r");
+    if(!fp){
+        printf("Error opening the aboutProject.txt file\n");
+        return;
+    }
+    char line[MAXBUFFERSIZE];
+    printf("--------------------------------------------------\n");
+    while(fgets(line, sizeof(line), fp) != NULL){
+        printf("%s", line);
+    }
+    fclose(fp);
+    printf("\n--------------------------------------------------\n");
+    return;
+}
