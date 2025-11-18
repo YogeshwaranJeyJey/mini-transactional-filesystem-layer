2826cd981dcde59db00fb8486e908852c1bc66e3
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-18 23:20:57 +0530

    Add aboutProject() function declaration and path macros

--- common.h
+++ common.h
@@ -4,14 +4,22 @@
 #define MAXCHOICECHAR   5
 #define WIDTH   50
 #define MAXBUFFERSIZE   256
-#define CHECKSUMFILEPATH    "checkSum.txt"
-#define WALFILEPATH "wal.txt"
+#define LAST_TXN_COUNTERID    1000
+#define TXN_COUNTER_TXTFILE_PATH   "txnCounter.txt"
+#define CHECKPOINT_FILE_PATH    "checkPoint.txt"
+#define WAL_TXTFILE_PATH  "wal.txt"
+#define NEW_WAL_TXTFILE_PATH    "newWal.txt"
+#define ACCOUNT_DETAILS_TXTFILE_PATH   "accountDetails.txt"
+#define ABOUT_PROJECT_FILE_PATH "aboutProject.txt"
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int doPadding(char* string);
 void trimNewLine(char* line);
+int isBlank(const char *s);
+void aboutProject();
 
 #endif
