00af6da34b08b0b0f6dc6f10e34171c5f1475d61
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-18 23:17:08 +0530

    Optimized bank services functions

--- bankServices.c
+++ bankServices.c
@@ -4,138 +4,126 @@
 
 accountDetails_t accDetails;
 
-int checkValidPassword(char* passwd, char* reTypedPasswd){
-    if(strcmp(passwd, reTypedPasswd) == 0) return 1;
-    return 0;
-}
-
-long generateNewAccNumber(){
-    long lastAccNumber = 100;
-    FILE* fp = fopen("accountNumberCounter.txt", "r");
-    if(!fp){
-        fp = fopen("accountNumberCounter.txt", "w");
-        if(!fp){
-            printf("Error creating accountNumberCounter.txt file!\n");
-            return -1;
-        }
-        lastAccNumber = 101;
-        fprintf(fp, "%ld", lastAccNumber);
-        fclose(fp);
-        return lastAccNumber;
-    }
-    fscanf(fp, "%ld", &lastAccNumber);
-    fclose(fp);
-    long newAccNumber = lastAccNumber + 1;
-
-    fp = fopen("accountNumberCounter.txt", "w");
-    if(!fp){
-        printf("Error occurred while writing the newAccNumber!\n");
-        return -1;
-    }
-    fprintf(fp, "%ld", newAccNumber);
-    fclose(fp);
-    return newAccNumber;
-}
-
 void addAccount(){
-    char passwd[MAXPASSWDCHAR];
-    char reTypedPasswd[MAXPASSWDCHAR];
-    int passwdRepeatFlag = 1;
-    int padding = doPadding(ADDACCOUNTHEADER);
-    printf("--------------------------------------------------\n");
-    printf("%*s%s%*s\n",padding, "",ADDACCOUNTHEADER, padding, "");
-
-    printf("Enter Name: ");
-    fgets(accDetails.name, sizeof(accDetails.name), stdin);
-    accDetails.name[strcspn(accDetails.name, "\n")] = '\0';
-
-    printf("Enter MobileNumber: ");
-    fgets(accDetails.mobileNumber, sizeof(accDetails.mobileNumber), stdin);
-    accDetails.mobileNumber[strcspn(accDetails.mobileNumber, "\n")] = '\0';
-
-    printf("Enter Mail Id: ");
-    fgets(accDetails.mailId, sizeof(accDetails.mailId), stdin);
-    accDetails.mailId[strcspn(accDetails.mailId, "\n")] = '\0';
-
-    while(passwdRepeatFlag){
-        printf("Set Password\n");
-        printf("Enter New Password (Max 6 characters): ");
-        fgets(passwd, sizeof(passwd), stdin);
-        passwd[strcspn(passwd, "\n")] = '\0';
-
-        printf("Confirm Password: ");
-        fgets(reTypedPasswd, sizeof(reTypedPasswd), stdin);
-        reTypedPasswd[strcspn(reTypedPasswd, "\n")] = '\0';
+    if(getNewAccountDetails(&accDetails)){
+        accDetails.accountNumber = generateNewAccNumber();
+        accDetails.accountBalance = 0.0;
         
-        if(checkValidPassword(passwd, reTypedPasswd)){
-            strcpy(accDetails.password, passwd);
-            printf("Password Set Successfully!\n");
-            break;
+        long txnId = generateTxnId();
+        if(txnId == -1){
+            return;
         }
-        else{
-            printf("Error: Passwords do not match!\n\n");
-        }
+        
+        writeCreateToWal(&accDetails, txnId, 0);
+        writeCommitToWAL(txnId);
+        writeAddToOriginalFile(&accDetails);
+        
+        printf("Account created successfully!\n");
     }
-    
-    accDetails.accountNumber = generateNewAccNumber();
-    accDetails.accountBalance = 0;
-
-    long txnId = generateTxnId();
-    if(txnId == -1){
+    else{
+        printf("Account creation failed!\n");
         return;
     }
-    writeCreateToWal(&accDetails, txnId);
-    writeAddToOriginalFile(&accDetails);
-    writeCommitToWAL(txnId);
 }
 
 void viewAccount(){
+    int padding = doPadding(VIEWACCOUNTHEADER);
+    
+    printf("--------------------------------------------------\n");
+    printf("%*s%s%*s\n",padding, "",VIEWACCOUNTHEADER, padding, "");
     if(validateAccountDetails(&accDetails)){
         printAccountDetails(&accDetails);
     }
-    else
+    else{
+        printf("Account Validation failed!\n");
         return;
+    }
+       
 }
 
 void updateAccountDetails(){
+    int padding = doPadding(UPDATEACCOUNTHEADER);
+    
+    printf("--------------------------------------------------\n");
+    printf("%*s%s%*s\n",padding, "",UPDATEACCOUNTHEADER, padding, "");
     if(validateAccountDetails(&accDetails)){
-        updateDetails(&accDetails);
+        if(updateDetails(&accDetails))
+            printf("Detail updated successfully!\n");
+        else
+             printf("Detail updation failed!\n");  
     }
-    else
+    else{
+        printf("Account Validation failed!\n");
         return;
+    }
 }
 
 void deleteAccount(){
+    int padding = doPadding(DELETEACCOUNTHEADER);
+    
+    printf("--------------------------------------------------\n");
+    printf("%*s%s%*s\n",padding, "",DELETEACCOUNTHEADER, padding, "");
     if(validateAccountDetails(&accDetails)){
         long txnId = generateTxnId();
         writeDeleteToWal(&accDetails, txnId);
-        writeDeleteToOriginalFile(&accDetails);
         writeCommitToWAL(txnId);
+        writeDeleteToOriginalFile(&accDetails);
+        
+        printf("Account deleted successfully!\n");
     }
-    else
+    else{
+        printf("Account Validation failed!\n");
         return;
+    }
 }
 
 void depositCash(){
+    int padding = doPadding(DEPOSITHEADER);
+    
+    printf("--------------------------------------------------\n");
+    printf("%*s%s%*s\n",padding, "",DEPOSITHEADER, padding, "");
     if(validateAccountDetails(&accDetails)){
-        deposit(&accDetails);
+        if(deposit(&accDetails))
+            printf("Cash deposited successfully!\n");
+        else
+            printf("Cash Not deposited!\n");
     }
-    else
+    else{
+        printf("Account Validation failed!\n");
         return;
+    }
 }
 
 void withDrawCash(){
+    int padding = doPadding(WITHDRAWHEADER);
+    
+    printf("--------------------------------------------------\n");
+    printf("%*s%s%*s\n",padding, "",WITHDRAWHEADER, padding, "");
     if(validateAccountDetails(&accDetails)){
-        withDraw(&accDetails);
+        if(withDraw(&accDetails))
+            printf("Cash withdrawn successfully!\n");
+        else
+           printf("Cash withdrawl failed!\n"); 
     }
-    else
+    else{
+        printf("Account Validation failed!\n");
         return;
+    }
 }
 
 void transferCash(){
+    int padding = doPadding(TRANSFERHEADER);
+    
+    printf("--------------------------------------------------\n");
+    printf("%*s%s%*s\n",padding, "",TRANSFERHEADER, padding, "");
     if(validateAccountDetails(&accDetails)){
-        transfer(&accDetails);
+        if(transfer(&accDetails))
+            printf("Cash transferred successfully!\n");
+        else
+            printf("Cash Not transferred!\n");
     }
-    else
+    else{
+        printf("Account Validation failed!\n");
         return;
+    }
 }
