19e8290a3e22b6edaad5868c33f96f4c751f7426
Yogeshwaran JeyJey <jjgowtham54@gmail.com>
2025-11-18 11:19:58 +0530

    Add crash recovery function headers

---
+++ crashRecovery.h
@@ -0,0 +1,35 @@
+#ifndef CRASHRECOVERY_H
+#define CRASHRECOVERY_H
+
+#define OPERATIONCHARSLEN   10
+
+#include "bankServices.h"
+
+typedef struct {
+    long txnId;
+    char operation[OPERATIONCHARSLEN];
+    int endTxnFlag;
+    int commitedFlag;
+    int hasOld;
+    int hasNew;
+    accountDetails_t oldAccDetails;
+    accountDetails_t newAccDetails;
+    int hasOldFrom, hasOldTo;
+    int hasNewFrom, hasNewTo;
+    accountDetails_t oldFromAccDetails;
+    accountDetails_t oldToAccDetails;
+    accountDetails_t newFromAccDetails;
+    accountDetails_t newToAccDetails;
+}wal_Txn_t;
+
+void startRecovery();
+int readWalTransaction(FILE* walFp, wal_Txn_t* walTxn);
+long getLastTxnId(char* checkSumPath);
+int isBlank(const char *s);
+int accountExists(long accNo);
+int undo(wal_Txn_t *walTxn);
+int redo(wal_Txn_t* walTxn);
+int updateCheckSum(char* checkSumPath, long lastTxnId);
+void printStruct(wal_Txn_t* walTxn);
+
+#endif
